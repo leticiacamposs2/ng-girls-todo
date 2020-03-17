fa607b30edfef04c37b7310c0cf1a411b5f7be4f
Leticia Campos <leticia.campos@totvs.com.br>
2020-03-17 12:48:32 -0300

    criação de um novo componente o todo-item que tem como finalidade mostrar todos os  itens presente na lista.  Será um componente simples no começo, mas irá crescer posteriormente. O  importante é que ele receberá o todo-item como uma entrada para o  componente pai. Deste modo poderá ser um componente reutilizável, e não  dependerá diretamente dos dados e estados da aplicação.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputButtonComponent } from './input-button/input-button.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputButtonComponent
+    InputButtonComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
