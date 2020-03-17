9c6005b611c155ebd7ec3201f602997e81563255
Leticia Campos <leticia.campos@totvs.com.br>
2020-03-17 11:32:58 -0300

    criação de novo componente input-button

---
+++ src/app/input-button/input-button.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-button',
+  templateUrl: './input-button.component.html',
+  styleUrls: ['./input-button.component.css']
+})
+export class InputButtonComponent implements OnInit {
+  title = '';
+
+  ngOnInit() {
+    this.title = 'Hello World';
+  }
+}
